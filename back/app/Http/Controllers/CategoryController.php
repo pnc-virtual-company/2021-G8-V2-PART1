3ed3a6be8cff232d0bccd444d8a95abca14181d6
SENKUNTHY <kunthy.sen@student.passerellesnumeriques.org>
2021-11-30 14:21:09 +0700

    Completed Manage Back End Category

---
+++ back/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Category;
+use App\Http\Resources\CategoryResource;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return CategoryResource::collection(Category::get()->take(5));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+        $category = new Category();
+        $category->name = $request->name;
+
+        $category->save();
+        return response()->json(['message'=>'Category Created', 'data'=> $category], 201);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return new CategoryResource(Category::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name'=>'required',
+        ]);
+        $category = Category::findOrFail($id);
+        $category->name = $request->name;
+
+        $category->save();
+        return response()->json(['massage'=>' Category Updated', 'data'=> $category], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $isDeleted = Category::destroy($id);
+        if($isDeleted == 1){
+            return response()->json(['massage'=>' Category Deleted'], 200);
+        }else{
+            return response()->json(['massage'=>'ID already exit '], 404);
+        }
+    }
+}
--- back/routes/api.php
+++ back/routes/api.php
@@ -3,7 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
 
 //user
 Route::post('/signup', [UserController::class, "signup"]);
 Route::post('/signin', [UserController::class, "signin"]);
+
+// category 
+
+Route::get('/categories', [CategoryController::class ,'index']);
+Route::get('/categories/{id}', [CategoryController::class ,'show']);
+
+Route::post('/categories', [CategoryController::class ,'store']);
+Route::put('/categories/{id}', [CategoryController::class ,'update']);
+Route::delete('/categories/{id}', [CategoryController::class ,'destroy']);
