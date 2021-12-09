d16d42f241ab4636dcef4cfb7b21dc0b70d99e5f
sreyngitdoeurm <sreyngit.doeurm@student.passerellesnumeriques.org>
2021-12-09 10:02:25 +0700

    #67 completed manage explore event component

---
+++ front/src/components/pages/event/exploreEvent/SearchFilter.vue
@@ -0,0 +1,114 @@
+<template>
+  <section>
+    <div class="add-and-search">
+      <div class="even-search">
+        <input
+          type="text"
+          class="searchKey"
+          placeholder="What are you looking for?"
+          v-model="keyWord"
+        />
+        <button type="button" class="searchButton" @click="clearSearch">
+          X
+        </button>
+      </div>
+      <div class="city-search">
+        <input
+          type="text"
+          class="city-searchKey"
+          placeholder="Not far from city"
+          v-model="keyWord"
+        />
+        <button type="button" class="city-searchButton" @click="clearSearch">
+          X
+        </button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    
+    };
+  },
+};
+</script>
+
+<style scoped>
+.add-and-search {
+  width: 82%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.btn-add {
+  margin: 0;
+  width: 150px;
+  background: var(--main-color);
+  border: none;
+  padding: 15px 20px;
+  border-radius: 50px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+}
+.btn-add:hover,
+.searchButton:hover, .city-searchButton:hover {
+  color: rgb(173, 101, 233);
+}
+
+.even-search {
+  width: 60%;
+  display: flex;
+}
+
+.searchKey {
+  width: 65%;
+  border: 2px solid var(--main-color);
+  border-right: none;
+  padding: 15px;
+  border-radius: 15px 0 0 15px;
+  outline: none;
+}
+
+.searchButton {
+  margin: 0;
+  width: 45px;
+  border: 1px solid var(--main-color);
+  background: var(--main-color);
+  color: #fff;
+  border-radius: 0 15px 15px 0;
+  cursor: pointer;
+  font-size: 24px;
+}
+
+.city-search{
+  width: 60%;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.city-searchKey {
+  width: 40%;
+  border: 2px solid rgb(34, 152, 207);
+  border-right: none;
+  padding: 15px;
+  border-radius: 15px 0 0 15px;
+  outline: none;
+}
+.city-searchButton {
+    margin: 0;
+  width: 45px;
+  border: 1px solid rgb(34, 152, 207);
+  background: rgb(34, 152, 207);
+  color: #fff;
+  border-radius: 0 15px 15px 0;
+  cursor: pointer;
+  font-size: 24px;
+}
+</style>
--- front/src/view/EventView.vue
+++ front/src/view/EventView.vue
@@ -1,86 +1,41 @@
 <template>
   <section>
-    <div class="even-search">
-        <input
-          type="text"
-          class="searchKey"
-          placeholder="What are you looking for?"
-          v-model="keyWord"
-        />
-        <button type="button" class="searchButton">
-          X
-        </button>
-    </div>
-    <!-- <add-search @showForm="showAddForm"></add-search> -->
+    <search-filter></search-filter>
     <my-event-card
       v-for="myEvent of this.myEvents"
       :key="myEvent.id"
       :myEvent="myEvent"
-      
+      :buttonMode="onEventMode"
     ></my-event-card>
-    <!------------------------- myevent card and from -->
   </section>
 </template>
 
 <script>
-import MyEventCard from "../components/pages/event/myevent/MyEventCard.vue";
-// import AddSearch from "../components/pages/event/myevent/AddSearch.vue";
-
+import SearchFilter from "../components/pages/event/exploreEvent/SearchFilter.vue";
+import MyEventCard from '../components/pages/event/myevent/MyEventCard.vue';
 import axios from "../axios-http.js";
 export default {
-  components: {MyEventCard},
-  props: ['userDataAppToEvent'],
+  components: { SearchFilter, MyEventCard },
   data() {
     return {
-        countriesCitiesInitial: [],
-        countriesCities: [],
-        categories: [],
-        myEvents: [],
+      myEvents: [],
+      onEventMode: 'event',
     };
   },
-  // 
-  computed: {
-  },
   methods: {
-    
-    getMyEventData(){
-      axios.get("/myevents")
+      getMyEventData(){
+      axios.get("api/myevents")
     .then( res => {
       this.myEvents = res.data
+
     })
     }
-     /// =======================crud=====================
-
   },
   mounted() {
-    // GET MYEVENT DATA FROM BACKEND
     this.getMyEventData()
   },
 };
 </script>
 
 <style scoped>
-.even-search {
-  width: 60%;
-  display: flex;
-}
-
-.searchKey {
-  width: 65%;
-  border: 2px solid var(--main-color);
-  border-right: none;
-  padding: 15px;
-  border-radius: 15px 0 0 15px;
-  outline: none;
-}
-.searchButton {
-  margin: 0;
-  width: 45px;
-  border: 1px solid var(--main-color);
-  background: var(--main-color);
-  color: #fff;
-  border-radius: 0 15px 15px 0;
-  cursor: pointer;
-  font-size: 24px;
-}
 </style>
